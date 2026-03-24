4d6a903e595fcfccc2d53f3bafd82df2ac57048d
Your Name <you@example.com>
2026-03-24 23:44:45 +0000

    Fix backward/forward arrows.

--- movies/src/components/headerMovieList/index.jsx
+++ movies/src/components/headerMovieList/index.jsx
@@ -1,10 +1,11 @@
+import { useNavigate } from "react-router";
 import React from "react";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 import Paper from "@mui/material/Paper";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
-import { useNavigate } from "react-router";
+
 
 
 const Header = (props ) => {
